8ddad31240fe33b8ec0dbda7e9abadcfbc675cab
Alper ARI <alper.hamza.ari@gmail.com>
2023-03-12 12:11:25 +0300

    Add 3 getMarketItemsByEventId() services in the market.dart

--- lib/services/market.dart
+++ lib/services/market.dart
@@ -6,6 +6,60 @@ import 'package:tickrypt/models/event_model.dart';
 class MarketService {
   var backendUrl = dotenv.env["BACKEND_URL"];
 
+  Future<List<dynamic>> getMarketItemsAllByEventId(eventId) async {
+    final String url =
+        '${backendUrl!}/market/market-items-all?eventId=$eventId';
+
+    var headers = {
+      "Access-Control-Allow-Origin": '*',
+    };
+
+    Response res = await get(
+      Uri.parse(url),
+      headers: headers,
+    );
+
+    var body = jsonDecode(res.body);
+
+    return body["marketItemsAll"];
+  }
+
+  Future<List<dynamic>> getMarketItemsOnSaleByEventId(eventId) async {
+    final String url =
+        '${backendUrl!}/market/market-items-onsale?eventId=$eventId';
+
+    var headers = {
+      "Access-Control-Allow-Origin": '*',
+    };
+
+    Response res = await get(
+      Uri.parse(url),
+      headers: headers,
+    );
+
+    var body = jsonDecode(res.body);
+
+    return body["marketItemsOnSale"];
+  }
+
+  Future<List<dynamic>> getMarketItemsSoldByEventId(eventId) async {
+    final String url =
+        '${backendUrl!}/market/market-items-sold?eventId=$eventId';
+
+    var headers = {
+      "Access-Control-Allow-Origin": '*',
+    };
+
+    Response res = await get(
+      Uri.parse(url),
+      headers: headers,
+    );
+
+    var body = jsonDecode(res.body);
+
+    return body["marketItemsSold"];
+  }
+
   Future<dynamic> sell(auth, tokenId, price) async {
     final String url = '${backendUrl!}/market/sell';
 
